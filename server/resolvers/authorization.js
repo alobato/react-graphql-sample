6b3d65aeb534f3a8462fe16ed689c7cc6a8f5016
Arley Lobato <arleylobato@gmail.com>
2018-09-01 19:49:52 -0300

    Correct authorization.js

--- server/resolvers/authorization.js
+++ server/resolvers/authorization.js
@@ -3,15 +3,9 @@ const { combineResolvers, skip } = require('graphql-resolvers')
 
 const isAuthenticated = (parent, args, { me }) => me ? skip : new ForbiddenError('Not authenticated as user.')
 
-exports.isAdmin = combineResolvers(
+const isAdmin = combineResolvers(
   isAuthenticated,
   (parent, args, { me: { role } }) => role === 'ADMIN' ? skip : new ForbiddenError('Not authorized as admin.'),
 )
 
-exports.isMessageOwner = async (parent, {id}, {models, me}) => {
-  const message = await models.Message.findById(id, { raw: true })
-  if (message.userId !== me.id) throw new ForbiddenError('Not authenticated as owner.')
-  return skip
-}
-
-exports.isAuthenticated
+module.exports = { isAuthenticated, isAdmin }
